f1ce2b351ba899a35776b13e90ebf5e23fb85ca9
pedrofelipegs <99354134+pedrofelipegs@users.noreply.github.com>
2022-03-23 08:26:07 -0300

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,41 @@
+function pegarDados1(){
+	let valor = 1;
+	switch(valor){
+		case 1:{
+			document.classList()
+		}
+	}
+}
+function pegarDados2(){
+	let valor = 2;
+	switch(valor){
+		case 2:{
+			document.querySelector(".result1").style.display = "block"
+			document.querySelector(".result2").style.display = "none"
+			document.querySelector(".result3").style.display = "none"
+			document.querySelector(".result4").style.display = "none"
+		}
+	}
+}
+function pegarDados3(){
+	let valor = 3;
+	switch(valor){
+		case 3:{
+			document.querySelector(".result1").style.display = "block"
+			document.querySelector(".result2").style.display = "none"
+			document.querySelector(".result3").style.display = "none"
+			document.querySelector(".result4").style.display = "none"
+		}
+	}
+}
+function pegarDados4(){
+	let valor = 4;
+	switch(valor){
+		case 4:{
+			document.querySelector(".result1").style.display = "block"
+			document.querySelector(".result2").style.display = "none"
+			document.querySelector(".result3").style.display = "none"
+			document.querySelector(".result4").style.display = "none"
+		}
+	}
+}
